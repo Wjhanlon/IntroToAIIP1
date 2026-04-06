3b9f0945e4dfa1ba0bbd5745d4ca2d090aba28c5
Wjhanlon <joeyhanlon44@gmail.com>
2026-04-06 18:38:24 -0400

    initial commit

---
+++ src/search.py
@@ -0,0 +1,35 @@
+from problem import Node
+from utils import memoize, PriorityQueue
+
+def astar_search(problem, h=None):
+    """A* search is best-first graph search with f(n) = g(n) + h(n).
+    You need to specify the h function when you call astar_search, or
+    else in your Problem subclass."""
+
+    #  __   __                  ____          _         _   _
+    #  \ \ / /__  _   _ _ __   / ___|___   __| | ___   | | | | ___ _ __ ___
+    #   \ V / _ \| | | | '__| | |   / _ \ / _` |/ _ \  | |_| |/ _ \ '__/ _ \
+    #    | | (_) | |_| | |    | |__| (_) | (_| |  __/  |  _  |  __/ | |  __/
+    #    |_|\___/ \__,_|_|     \____\___/ \__,_|\___|  |_| |_|\___|_|  \___|
+
+    h = memoize(h or problem.h)
+    # Be sure to read about hints in the IP 1 description.
+
+
+
+    return None
+
+def ucs_search(problem):
+    """Uniform Cost Search (UCS) is a search algorithm that expands the least
+    cost node in the search tree. It is a special case of A* search, so try to
+    reuse the astar_search function."""
+
+    #  __   __                  ____          _         _   _
+    #  \ \ / /__  _   _ _ __   / ___|___   __| | ___   | | | | ___ _ __ ___
+    #   \ V / _ \| | | | '__| | |   / _ \ / _` |/ _ \  | |_| |/ _ \ '__/ _ \
+    #    | | (_) | |_| | |    | |__| (_) | (_| |  __/  |  _  |  __/ | |  __/
+    #    |_|\___/ \__,_|_|     \____\___/ \__,_|\___|  |_| |_|\___|_|  \___|
+
+
+
+    return None
